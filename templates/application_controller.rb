b63009a4e01ba2952e9762a2462ae5399a869e91
Cecile Veneziani <zora@patapouf.org>
2015-02-25 01:27:00 +0100

    Enhance application controller
    - Order callbacks and includes
    - Use before_action instead of before_filter

--- templates/application_controller.rb
+++ templates/application_controller.rb
@@ -1,19 +1,16 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  before_filter :authenticate_user!, unless: :pages_controller?
-
   include Pundit
-  after_action :verify_authorized, except: :index, unless: :devise_or_pages_controller?
+
+  protect_from_forgery with: :exception
+
+  before_action :authenticate_user!, unless: :pages_controller?
+
+  after_action :verify_authorized, except:  :index, unless: :devise_or_pages_controller?
   after_action :verify_policy_scoped, only: :index, unless: :devise_or_pages_controller?
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   private
-
-  def user_not_authorized
-    flash[:error] = I18n.t('controllers.application.user_not_authorized', default: "You can't access this page.")
-    redirect_to(root_path)
-  end
 
   def devise_or_pages_controller?
     devise_controller? || pages_controller?
@@ -21,5 +18,10 @@ class ApplicationController < ActionController::Base
 
   def pages_controller?
     controller_name == "pages"  # Brought by the `high_voltage` gem
+  end
+
+  def user_not_authorized
+    flash[:error] = I18n.t('controllers.application.user_not_authorized', default: "You can't access this page.")
+    redirect_to(root_path)
   end
 end
