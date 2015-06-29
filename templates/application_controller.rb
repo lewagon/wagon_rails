0a1962aa5aa0475a008af1080e34a19afe98e9e6
Sebastien Saunier <seb@saunier.me>
2015-06-29 23:36:14 +0200

    Uncomment before_action for pundit

--- templates/application_controller.rb
+++ templates/application_controller.rb
@@ -5,10 +5,10 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!, unless: :pages_controller?
 
-  after_action :verify_authorized, except:  :index, unless: :devise_or_pages_controller?
-  after_action :verify_policy_scoped, only: :index, unless: :devise_or_pages_controller?
+  # after_action :verify_authorized, except:  :index, unless: :devise_or_pages_controller?
+  # after_action :verify_policy_scoped, only: :index, unless: :devise_or_pages_controller?
 
-  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  # rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   private
 
