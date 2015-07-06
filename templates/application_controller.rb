6dcbf00b77e61c4698a50cb9ed74840720161f85
Sebastien Saunier <seb@saunier.me>
2015-07-06 14:31:27 +0200

    Completely remove pundit

--- templates/application_controller.rb
+++ templates/application_controller.rb
@@ -1,19 +1,19 @@
 class ApplicationController < ActionController::Base
-  include Pundit
 
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!, unless: :pages_controller?
 
-  # after_action :verify_authorized, except:  :index, unless: :devise_or_pages_controller?
-  # after_action :verify_policy_scoped, only: :index, unless: :devise_or_pages_controller?
-
+  # Uncomment these lines to get pundit
+  # include Pundit
+  # after_action :verify_authorized, except:  :index, unless: :devise_or_pages_or_admin_controller?
+  # after_action :verify_policy_scoped, only: :index, unless: :devise_or_pages_or_admin_controller?
   # rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   private
 
-  def devise_or_pages_controller?
-    devise_controller? || pages_controller?
+  def devise_or_pages_or_admin_controller?
+    devise_controller? || pages_controller? || params[:controller] =~ /^admin/
   end
 
   def pages_controller?
